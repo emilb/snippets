778b880a0d76060b7f3ecf2353a9744b780dee37
Emil Breding <emil.breding@viaplay.com>
2015-07-08 15:35:23 +0200

    Added Kodi clean/refresh scripts

---
+++ kodi/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage:
+# clean.sh <ip>
+#
+# Cleans the library for the Kodi running at <ip>
+#
+
+which curl &> /dev/null || { printf 'Missing required curl\n'; exit 1; }
+
+xbmcAddress=$1
+
+#curl -s --data-binary '{ "id": 1, "jsonrpc": "2.0", "method": "VideoLibrary.GetMovies" }' -H 'content-type: application/json;' http://$xbmcAddress:80/jsonrpc | jq '.'
+
+curl -s --data-binary '{ "id": 1, "jsonrpc": "2.0", "method": "VideoLibrary.Scan" }' -H 'content-type: application/json;' http://$xbmcAddress:80/jsonrpc | jq '.'
